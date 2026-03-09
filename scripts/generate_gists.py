362a24c83a2279fac3b397c6a0c5e34580d0a8a3
Adam Parasion <101869636+aparasion@users.noreply.github.com>
2026-03-09 14:32:37 +0100

    Update generate_gists.py

--- scripts/generate_gists.py
+++ scripts/generate_gists.py
@@ -31,7 +31,6 @@ FEEDS = [
     "https://aparasion.github.io/rss-generator/rss/phrase-blog.xml",
     "https://aparasion.github.io/rss-generator/rss/lokalise-blog.xml",
     "https://aparasion.github.io/rss-generator/rss/crowdin-blog.xml",
-    "https://aparasion.github.io/rss-generator/rss/DeepL-blog.xml",
 ]
 
 SEEN_FILE = "seen.json"
